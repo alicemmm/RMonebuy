3abb2408e22e88dfad1f3edefdb2fea9bf694e54
asia <345646798@qq.com>
2016-11-23 18:15:10 +0800

    add fragment

--- app/src/main/java/mmm/asia/rmonebuy/MainActivity.java
+++ app/src/main/java/mmm/asia/rmonebuy/MainActivity.java
@@ -1,11 +1,9 @@
 package mmm.asia.rmonebuy;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -18,9 +16,7 @@ import me.majiajie.pagerbottomtabstrip.listener.OnTabItemSelectListener;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    int[] testColors = {0xFF00796B, 0xFF5B4947, 0xFF607D8B, 0xFFF57C00, 0xFFF57C00};
-
-    private Controller controller;
+    int[] mainTabColors = {0xFF00796B, 0xFF5B4947, 0xFF607D8B, 0xFFF57C00, 0xFFF57C00};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,27 +25,32 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        BottomTabTest();
+        initFragment();
+
+        BottomTabInit();
+    }
+
+    private void initFragment() {
 
     }
 
-    private void BottomTabTest() {
-        PagerBottomTabLayout pagerBottomTabLayout = (PagerBottomTabLayout) findViewById(R.id.tab);
+    private void BottomTabInit() {
+        PagerBottomTabLayout pagerBottomTabLayout = (PagerBottomTabLayout) findViewById(R.id.main_pager_bottom_tab_layout);
 
         //用TabItemBuilder构建一个导航按钮
         TabItemBuilder tabItemBuilder = new TabItemBuilder(this).create()
                 .setDefaultIcon(android.R.drawable.ic_menu_send)
                 .setText("信息")
-                .setSelectedColor(testColors[0])
+                .setSelectedColor(mainTabColors[0])
                 .setTag("A")
                 .build();
 
         //构建导航栏,得到Controller进行后续控制
-        controller = pagerBottomTabLayout.builder()
+        Controller controller = pagerBottomTabLayout.builder()
                 .addTabItem(tabItemBuilder)
-                .addTabItem(android.R.drawable.ic_menu_compass, "位置", testColors[1])
-                .addTabItem(android.R.drawable.ic_menu_search, "搜索", testColors[2])
-                .addTabItem(android.R.drawable.ic_menu_help, "帮助", testColors[3])
+                .addTabItem(android.R.drawable.ic_menu_compass, "位置", mainTabColors[1])
+                .addTabItem(android.R.drawable.ic_menu_search, "搜索", mainTabColors[2])
+                .addTabItem(android.R.drawable.ic_menu_help, "帮助", mainTabColors[3])
 //                .setMode(TabLayoutMode.HIDE_TEXT)
                 .setMode(TabLayoutMode.CHANGE_BACKGROUND_COLOR)
 //                .setMode(TabLayoutMode.HIDE_TEXT| TabLayoutMode.CHANGE_BACKGROUND_COLOR)
---
+++ app/src/main/java/mmm/asia/rmonebuy/fragment/AFragment.java
@@ -0,0 +1,93 @@
+package mmm.asia.rmonebuy.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import mmm.asia.rmonebuy.R;
+
+public class AFragment extends Fragment {
+    public static final String TAG = AFragment.class.getSimpleName();
+
+    private boolean isViewFirstAppear = true;
+
+    private String argument;
+
+    private TextView textView;
+
+    public AFragment() {
+    }
+
+    public static AFragment getInstance(String argument) {
+        Bundle args = new Bundle();
+        args.putString("argument", argument);
+
+        AFragment fragment = new AFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            argument = bundle.getString("argument", "");
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_a, container);
+        textView = (TextView) view.findViewById(R.id.fragment_a_title_tv);
+        textView.setText(argument);
+
+        return view;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (getView() != null) {
+            if (isVisibleToUser)
+                onViewAppear();
+            else
+                onViewDisappear();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (getUserVisibleHint())
+            onViewAppear();
+        else
+            onViewDisappear();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        onViewDisappear();
+    }
+
+    public void onViewFirstAppear() {
+    }
+
+    public void onViewDisappear() {
+    }
+
+
+    public void onViewAppear() {
+        if (isViewFirstAppear) {
+            onViewFirstAppear();
+            isViewFirstAppear = false;
+        }
+    }
+
+}
