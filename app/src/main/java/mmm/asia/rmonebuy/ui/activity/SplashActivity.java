f6d6209d1568f5d4f556542b19d8c022da7611a0
asia <345646798@qq.com>
2016-11-25 16:08:59 +0800

    add splash activity

---
+++ app/src/main/java/mmm/asia/rmonebuy/ui/activity/SplashActivity.java
@@ -0,0 +1,40 @@
+package mmm.asia.rmonebuy.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+
+import mmm.asia.rmonebuy.R;
+
+/**
+ * Created by asia on 25/11/2016.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    private static final String TAG = SplashActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                SplashActivity.this.finish();
+            }
+        }, 1000);
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK)
+            return true;
+        return super.onKeyDown(keyCode, event);
+    }
+}
