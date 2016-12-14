bf76895eee4b449a8243e397bf966fd535729ac7
asia <345646798@qq.com>
2016-12-14 18:33:38 +0800

    add bmob

---
+++ app/src/main/java/mmm/asia/rmonebuy/bean/BaseUser.java
@@ -0,0 +1,118 @@
+package mmm.asia.rmonebuy.bean;
+
+import cn.bmob.v3.BmobObject;
+
+/**
+ * Created by asia on 12/12/2016.
+ */
+
+public class BaseUser extends BmobObject {
+    private Integer id;
+    private String imei;
+    private Integer versionCode;
+    private String versionName;
+    private String nets;// 网络接入商
+    private String netType;//网络类型
+    private Integer screenWidth;
+    private Integer screenHeight;
+    private String phoneType;
+    private String androidVersion;
+    private String other1;
+    private String other2;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getImei() {
+        return imei;
+    }
+
+    public void setImei(String imei) {
+        this.imei = imei;
+    }
+
+    public Integer getVersionCode() {
+        return versionCode;
+    }
+
+    public void setVersionCode(Integer versionCode) {
+        this.versionCode = versionCode;
+    }
+
+    public String getVersionName() {
+        return versionName;
+    }
+
+    public void setVersionName(String versionName) {
+        this.versionName = versionName;
+    }
+
+    public String getNets() {
+        return nets;
+    }
+
+    public void setNets(String nets) {
+        this.nets = nets;
+    }
+
+    public String getNetType() {
+        return netType;
+    }
+
+    public void setNetType(String netType) {
+        this.netType = netType;
+    }
+
+    public Integer getScreenWidth() {
+        return screenWidth;
+    }
+
+    public void setScreenWidth(Integer screenWidth) {
+        this.screenWidth = screenWidth;
+    }
+
+    public Integer getScreenHeight() {
+        return screenHeight;
+    }
+
+    public void setScreenHeight(Integer screenHeight) {
+        this.screenHeight = screenHeight;
+    }
+
+    public String getPhoneType() {
+        return phoneType;
+    }
+
+    public void setPhoneType(String phoneType) {
+        this.phoneType = phoneType;
+    }
+
+    public String getAndroidVersion() {
+        return androidVersion;
+    }
+
+    public void setAndroidVersion(String androidVersion) {
+        this.androidVersion = androidVersion;
+    }
+
+    public String getOther1() {
+        return other1;
+    }
+
+    public void setOther1(String other1) {
+        this.other1 = other1;
+    }
+
+    public String getOther2() {
+        return other2;
+    }
+
+    public void setOther2(String other2) {
+        this.other2 = other2;
+    }
+}
